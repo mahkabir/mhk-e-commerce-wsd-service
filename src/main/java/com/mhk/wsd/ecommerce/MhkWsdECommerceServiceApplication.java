88fe458415ea81d379116ce5b774cd5d3803928c
Humayun <red@1234567>
2024-10-09 00:14:11 +0600

    Initial commit

---
+++ src/main/java/com/mhk/wsd/ecommerce/MhkWsdECommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mhk.wsd.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhkWsdECommerceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhkWsdECommerceServiceApplication.class, args);
+	}
+
+}
